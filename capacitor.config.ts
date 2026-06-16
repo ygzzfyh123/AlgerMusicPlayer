7caf00be3e80bd401da7d093ae81bdc0cb2f441e
ygzzfyh123 <ygzzfyh2011422@gmail.com>
2026-06-16 17:24:15 +0800

    ci: enable android apk build and add capacitor project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alger.music',
+  appName: 'AlgerMusicPlayer',
+  webDir: 'out/renderer'
+};
+
+export default config;
